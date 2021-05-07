c57b31ef4323b96bf3a3a64eb0affbcc797d23c5
Ulysses Lin <utemoc@gmail.com>
2021-05-07 14:25:28 -0700

    Adds more RegisterUser, Technician, Client, Discount sample data

--- SalonSpace/createDB/createSalonSpaceSampleData.js
+++ SalonSpace/createDB/createSalonSpaceSampleData.js
@@ -64,8 +64,89 @@ usersCollection.insert(
 	loginStatus: false
 }
 )
+usersCollection.insert(
+{
+	registeredUserID: 3,
+	email: "deshaun@gmail.com",
+	firstName: "DeShaun",
+	lastName: "Jamieson",
+	password: "testdeshaun",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 4,
+	email: "katiegoldstein@gmail.com",
+	firstName: "Katie",
+	lastName: "Goldstein",
+	password: "testkatie",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 5,
+	email: "kenmiyamoto@gmail.com",
+	firstName: "Ken",
+	lastName: "Miyamoto",
+	password: "testken",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 6,
+	email: "rupikaur@gmail.com",
+	firstName: "Rupi",
+	lastName: "Kaur",
+	password: "testrupi",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 7,
+	email: "candacehernandez@gmail.com",
+	firstName: "Candace",
+	lastName: "Hernandez",
+	password: "testcandace",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 8,
+	email: "margaretthompson@gmail.com",
+	firstName: "Margaret",
+	lastName: "Thompson",
+	password: "testmargaret",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 9,
+	email: "denahassan@gmail.com",
+	firstName: "Dena",
+	lastName: "Hassan",
+	password: "testdena",
+	loginStatus: false
+}
+)
+usersCollection.insert(
+{
+	registeredUserID: 10,
+	email: "jamekafreeman@gmail.com",
+	firstName: "Jameka",
+	lastName: "Freeman",
+	password: "testjameka",
+	loginStatus: false
+}
+)
 
 // TECHNICIANS
+// skillListID, ratingListID, salonListID, languageListID may need to be arrays
 db.createCollection('technicians')
 techniciansCollection = db.getCollection("technicians")
 techniciansCollection.remove({})
@@ -83,14 +164,43 @@ techniciansCollection.insert(
 {
 	registeredUserID: 2,
 	technicianID: 2,
-	name: "Anya",
-	age: 30,
 	skillListID: 2,
 	ratingListID: 2,
 	salonListID: 2,
 	languageListID: 2
 }
 )
+techniciansCollection.insert(
+{
+	registeredUserID: 5,
+	technicianID: 3,
+	skillListID: 2,
+	ratingListID: 2,
+	salonListID: 2,
+	languageListID: 2
+}
+)
+techniciansCollection.insert(
+{
+	registeredUserID: 7,
+	technicianID: 4,
+	skillListID: 2,
+	ratingListID: 2,
+	salonListID: 2,
+	languageListID: 2
+}
+)
+techniciansCollection.insert(
+{
+	registeredUserID: 10,
+	technicianID: 5,
+	skillListID: 2,
+	ratingListID: 2,
+	salonListID: 2,
+	languageListID: 2
+}
+)
+
 
 // LANGUAGES
 db.createCollection('languages')
@@ -159,6 +269,38 @@ clientsCollection.insert(
         discountListID: [1]
     }
 )
+clientsCollection.insert(
+    {
+        registeredUserID: 4,
+        points: 87,
+        ratingListID: [4],
+        discountListID: [2, 3, 4]
+    }
+)
+clientsCollection.insert(
+    {
+        registeredUserID: 6,
+        points: 0,
+        ratingListID: [], // new user, has not rated yet
+        discountListID: []
+    }
+)
+clientsCollection.insert(
+    {
+        registeredUserID: 8,
+        points: 50,
+        ratingListID: [5, 6],
+        discountListID: [5, 6, 7, 8]
+    }
+)
+clientsCollection.insert(
+    {
+        registeredUserID: 9,
+        points: 0,
+        ratingListID: [], // rating was deleted
+        discountListID: [9, 10]
+    }
+)
 
 // DISCOUNTS
 db.createCollection('discounts')
@@ -185,4 +327,52 @@ discountsCollection.insert(
         used: true
     }
 )
-
+discountsCollection.insert(
+    {
+        discountID: 4,
+        value: 5.00,
+        used: false
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 5,
+        value: 25.00,
+        used: true
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 6,
+        value: 25.00,
+        used: false
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 7,
+        value: 10.00,
+        used: true
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 8,
+        value: 5.00,
+        used: false
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 9,
+        value: 25.00,
+        used: true
+    }
+)
+discountsCollection.insert(
+    {
+        discountID: 10,
+        value: 25.00,
+        used: false
+    }
+)
