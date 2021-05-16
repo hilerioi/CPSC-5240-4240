b8da855d786df3788f104ee5eb0fb281ca675695
Anh Tran <ahtran7@gmail.com>
2021-05-16 08:31:06 -0700

    Changed skillListID in technician inteface to array<number>

--- SalonSpace/interfaces/ITechnicianModel.ts
+++ SalonSpace/interfaces/ITechnicianModel.ts
@@ -3,7 +3,7 @@ import Mongoose = require("mongoose");
 interface ITechnicianModel extends Mongoose.Document {
     registeredUserID: number;
     technicianID: number;
-    skillListID: number;
+    skillListID: Array<number>;
     ratingListID: number;
     salonListID: number;
     languageListID: number;
