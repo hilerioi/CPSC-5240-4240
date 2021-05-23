b4e227b96b57c8405c70b31f340827c52918a6e3
orloffn <ameofoxil@gmail.com>
2021-05-22 18:50:18 -0700

    Welcome Page

--- SalonSpaceAngular/src/app/app-routing.module.ts
+++ SalonSpaceAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnicianListComponent } from './technician-list/technician-list.component';
+import { WelcomeComponent} from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'technician', component: TechnicianListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
